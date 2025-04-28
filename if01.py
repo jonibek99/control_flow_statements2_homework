f056cab32ca5241f4031c234caff6dea4920950c
jonibek99 <jonibekannaev5@gmail.com>
2025-04-28 19:09:34 +0500

    if1

--- if01.py
+++ if01.py
@@ -8,4 +8,12 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+#     if a>b and a>c:
+#         return a
+#     elif b>a and b>c:
+#         return b
+#     return c
+# print(main(int(input()),int(input()),int(input())))
+    return  max(a,b,c)
+print(main(int(input()),int(input()),int(input())))
+
